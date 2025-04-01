8c518296c6e71a9e3967e5579c35a1e1f7dfbae8
KarkalaTejasPai <Karkala.Pai@cognizant.com>
2025-04-01 10:38:32 +0530

    OnModelBuildingCode

--- OAS-ClassLib/AppDbContext.cs
+++ OAS-ClassLib/AppDbContext.cs
@@ -15,6 +15,29 @@ namespace OAS_ClassLib
             optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=OAS;Integrated Security=True;TrustServerCertificate=True");
         }
 
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Review>(entity =>
+        //    {
+        //        entity.ToTable("Review");
+        //        entity.HasKey(e => e.ReviewID);
+        //        entity.HasOne(e => e.User)
+        //              .WithMany(u => u.Reviews)
+        //              .HasForeignKey(e => e.UserID);
+        //    });
+        //    modelBuilder.Entity<Bid>(entity =>
+        //    {
+        //        entity.ToTable("Bids");
+        //        entity.HasKey(e => e.BidID);
+        //        entity.HasOne(e => e.Auction)
+        //              .WithMany(a => a.Bids)
+        //              .HasForeignKey(e => e.AuctionID);
+        //    entity.HasOne(e => e.Buyer)
+        //              .WithMany(u => u.Bids)
+        //              .HasForeignKey(e => e.BuyerID);
+        //    });
+        //}
+
         public DbSet<Product> Products { get; set; }
         public DbSet<Bid> Bids { get; set; }
         public DbSet<Review> Reviews { get; set; }
